02d01bc34ae154d401d81212f496add6ab5d925d
Eric Polman <eric@ericpolman.com>
2018-03-22 13:09:26 +0100

    Re-added env files.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC56KYcAIW4ORcnlco30IPnfHTR_vG9Ksw',
+    authDomain: 'posterbox-90096.firebaseapp.com',
+    databaseURL: 'https://posterbox-90096.firebaseio.com',
+    projectId: 'posterbox-90096',
+    storageBucket: 'posterbox-90096.appspot.com',
+    messagingSenderId: '649623494550'
+  }
+};

1c82816496d3d7062c2c8742884f2253786c3025
Eric Polman <eric@ericpolman.com>
2018-03-25 23:28:26 +0200

    bugfix and basic caching

--- src/app/shared/posters.service.ts
+++ src/app/shared/posters.service.ts
@@ -8,7 +8,7 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class PostersService {
   posters = new Array<Poster>();
-  postersLoaded: Promise<boolean>;
+  postersLoaded: boolean;
 
 
   constructor(private authService: AuthenticationService, private httpClient: HttpClient) {
@@ -19,42 +19,51 @@ export class PostersService {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
   }
 
-  getPosters(tag: string, searchQuery: string) {
-    const headers = new HttpHeaders({'x-access-token': this.authService.credentials.token});
-
-    return this.httpClient.get(environment.apiUrl + '/posters', {headers: headers}).toPromise()
-      .then((posters: PosterPost[]) => {
-        posters.forEach((poster) => {
-          const p = new Poster();
-          p.title = poster.title;
-          p.customerId = poster.customerId;
-          p.thumbnail = poster.thumbnail;
-          p.tags = JSON.parse(poster.tags);
-          p.files = JSON.parse(poster.files);
-          this.posters.push(p);
-        });
-
-        let result = this.posters.slice();
-        if (tag.length > 0) {
-          result = result.filter((poster: Poster) => {
-            if (poster.tags.includes(tag)) {
-              return true;
-            } else {
-              return false;
-            }
-          });
+  _filterPosters(tag: string, searchQuery: string) {
+    let result = this.posters.slice();
+    if (tag.length > 0) {
+      result = result.filter((poster: Poster) => {
+        if (poster.tags.includes(tag)) {
+          return true;
+        } else {
+          return false;
         }
-        if (searchQuery.length > 0) {
-          result = result.filter((poster: Poster) => {
-            if (JSON.stringify(poster).toLowerCase().includes(searchQuery.toLowerCase())) {
-              return true;
-            } else {
-              return false;
-            }
-          });
-        }
-        return result;
       });
+    }
+    if (searchQuery.length > 0) {
+      result = result.filter((poster: Poster) => {
+        if (JSON.stringify(poster).toLowerCase().includes(searchQuery.toLowerCase())) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    }
+    return result;
+  }
 
+  getPosters(tag: string, searchQuery: string) {
+    if (this.postersLoaded) {
+      return new Promise<Poster[]>((resolve, reject) => resolve(this._filterPosters(tag, searchQuery)));
+    } else {
+      const headers = new HttpHeaders({'x-access-token': this.authService.credentials.token});
+
+      return this.httpClient.get(environment.apiUrl + '/posters', {headers: headers}).toPromise()
+        .then((posters: PosterPost[]) => {
+          this.posters = [];
+          posters.forEach((poster) => {
+            const p = new Poster();
+            p.title = poster.title;
+            p.customerId = poster.customerId;
+            p.thumbnail = poster.thumbnail;
+            p.tags = JSON.parse(poster.tags);
+            p.files = JSON.parse(poster.files);
+            this.posters.push(p);
+          });
+
+          this.postersLoaded = true;
+          return this._filterPosters(tag, searchQuery);
+        });
+    }
   }
 }
